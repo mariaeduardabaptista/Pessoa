0c208cb41d12eca3257f27dda6bf2e453755f95d
Dudinha <dudabaptista14@gmail.com>
2021-03-23 00:55:09 -0300

    terceiro commit

--- src/Classe/Funcionario.java
+++ src/Classe/Funcionario.java
@@ -1,5 +1,10 @@
 package Classe;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+
+    public Funcionario(String nome, String cpf) {
+        super(nome, cpf);
+
+    } 
     
 }
--- src/Classe/Professor.java
+++ src/Classe/Professor.java
@@ -1,5 +1,9 @@
 package Classe;
 
-public class Professor {
+public class Professor extends Pessoa {
+
+    public Professor(String nome, String cpf) {
+        super(nome, cpf);
+    }
     
 }
